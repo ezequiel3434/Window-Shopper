37fc0d189a3783879bf711ac0b6c09b8613e378b
Ezequiel <ezequiel3434@gmail.com>
2020-07-16 12:05:36 -0300

    adding constraints text fields with custom view

---
+++ Window-Shopper/Views/CurrencyTxtField.swift
@@ -0,0 +1,29 @@
+//
+//  CurrencyTxtField.swift
+//  Window-Shopper
+//
+//  Created by Ezequiel Parada Beltran on 16/07/2020.
+//  Copyright © 2020 Ezequiel Parada. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTxtField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+       
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: UIColor.white])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+        
+
+    }
+    
+}
