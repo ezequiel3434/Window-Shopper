ed71f475db8d938b11991ea876f57a30ecae8af8
Ezequiel <ezequiel3434@gmail.com>
2020-07-17 19:05:52 -0300

    Custom drawing

--- Window-Shopper/Views/CurrencyTxtField.swift
+++ Window-Shopper/Views/CurrencyTxtField.swift
@@ -11,6 +11,22 @@ import UIKit
 @IBDesignable
 class CurrencyTxtField: UITextField {
     
+    override func draw(_ rect: CGRect) {
+        let size: CGFloat = 20
+        let currencyLabel = UILabel(frame: CGRect(x: 5, y: (frame.size.height/2) - size/2, width: size, height: size))
+        currencyLabel.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 0.795697774)
+        currencyLabel.textAlignment = .center
+        currencyLabel.textColor = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        currencyLabel.layer.cornerRadius = 5.0
+        currencyLabel.clipsToBounds = true
+        
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.locale = Locale(identifier: "en_US")
+        currencyLabel.text = formatter.currencySymbol
+        addSubview(currencyLabel)
+    }
+    
     override func prepareForInterfaceBuilder() {
         customizeView()
     }
@@ -26,6 +42,8 @@ class CurrencyTxtField: UITextField {
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
          layer.cornerRadius = 5.0
          textAlignment = .center
+        
+        clipsToBounds = true
          
         
          if let p = placeholder {
